6a6c5caf81787fb261b13d364c66d33dd0f72e3a
Gabriel Santos <ga_briel.santos1@outlook.com>
2023-09-19 23:22:58 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -45,6 +46,13 @@ public class UI {
         catch (RuntimeException e) {
             throw new InputMismatchException("Erro ao instanciar ChessPosition. Os valores validos são de a1 a h8.");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turno: " + chessMatch.getTurn());
+        System.out.println("Esperando player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
